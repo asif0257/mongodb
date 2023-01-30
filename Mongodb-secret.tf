4fb9c77c19a55dfe2e0f059cc83864ac4aae52d8
pravin135 <thotapraveenbabu@gmail.com>
2023-01-30 12:02:08 +0530

    added mongodb

--- Mongodb-secret.tf
+++ Mongodb-secret.tf
@@ -1,13 +1,13 @@
-# resource "kubernetes_secret" "mongodb_secret" {
-#   metadata {
-#     name = "mongodb-secret"
-#   }
+resource "kubernetes_secret" "mongodb_secret" {
+  metadata {
+    name = "mongodb-secret"
+  }
 
-#   data = {
-#     mongo-root-password = "cGFzc3dvcmQ="
+  data = {
+    mongo-root-password = "cGFzc3dvcmQ="
 
-#     mongo-root-username = "dXNlcm5hbWU="
-#   }
+    mongo-root-username = "dXNlcm5hbWU="
+  }
 
-#   type = "Opaque"
-# }
+  type = "Opaque"
+}
