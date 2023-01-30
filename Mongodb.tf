b4a6c307ce41cb551171262e83e799509db3ba5f
pravin135 <thotapraveenbabu@gmail.com>
2023-01-30 11:40:24 +0530

    added mongodb

--- Mongodb.tf
+++ Mongodb.tf
@@ -1,81 +1,81 @@
-# resource "kubernetes_deployment" "mongodb_deployment" {
-#   metadata {
-#     name = "mongodb-deployment"
+resource "kubernetes_deployment" "mongodb_deployment" {
+  metadata {
+    name = "mongodb-deployment"
 
-#     labels = {
-#       app = "mongodb"
-#     }
-#   }
+    labels = {
+      app = "mongodb"
+    }
+  }
 
-#   spec {
-#     replicas = 1
+  spec {
+    replicas = 1
 
-#     selector {
-#       match_labels = {
-#         app = "mongodb"
-#       }
-#     }
+    selector {
+      match_labels = {
+        app = "mongodb"
+      }
+    }
 
-#     template {
-#       metadata {
-#         labels = {
-#           app = "mongodb"
-#         }
-#       }
+    template {
+      metadata {
+        labels = {
+          app = "mongodb"
+        }
+      }
 
-#       spec {
-#         container {
-#           name  = "mongodb"
-#           image = "mongo"
+      spec {
+        container {
+          name  = "mongodb"
+          image = "mongo"
 
-#           port {
-#             container_port = 27017
-#           }
+          port {
+            container_port = 27017
+          }
 
-#           env {
-#             name = "MONGO_INITDB_ROOT_USERNAME"
+          env {
+            name = "MONGO_INITDB_ROOT_USERNAME"
 
-#             value_from {
-#               secret_key_ref {
-#                 name = "mongodb-secret"
-#                 key  = "mongo-root-username"
-#               }
-#             }
-#           }
+            value_from {
+              secret_key_ref {
+                name = "mongodb-secret"
+                key  = "mongo-root-username"
+              }
+            }
+          }
 
-#           env {
-#             name = "MONGO_INITDB_ROOT_PASSWORD"
+          env {
+            name = "MONGO_INITDB_ROOT_PASSWORD"
 
-#             value_from {
-#               secret_key_ref {
-#                 name = "mongodb-secret"
-#                 key  = "mongo-root-password"
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+            value_from {
+              secret_key_ref {
+                name = "mongodb-secret"
+                key  = "mongo-root-password"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
-# resource "kubernetes_service" "mongodb_service" {
-#   metadata {
-#     name = "mongodb-service"
-#   }
+resource "kubernetes_service" "mongodb_service" {
+  metadata {
+    name = "mongodb-service"
+  }
 
-#   spec {
-#     port {
-#       protocol    = "TCP"
-#       port        = 27017
-#       target_port = "27017"
-#     }
+  spec {
+    port {
+      protocol    = "TCP"
+      port        = 27017
+      target_port = "27017"
+    }
 
-#     selector = {
-#       app = "mongodb"
-#     }
-#   }
-#   depends_on = [
-#     google_container_cluster.primary
-#   ]
-# }
+    selector = {
+      app = "mongodb"
+    }
+  }
+  depends_on = [
+    google_container_cluster.primary
+  ]
+}
